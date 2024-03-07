6dcdc89c0be191e09f18bf0f70d604dbb209e25e
Ginasone <127901244+Ginasone@users.noreply.github.com>
2024-03-07 21:40:25 +0000

    Add files via upload

---
+++ schemas/user.ts
@@ -0,0 +1,55 @@
+import { defineField } from "sanity";
+
+const user={
+    name:"user",
+    title:"User",
+    type:"document",
+    fields:[
+        defineField({
+            name:"isAdmin",
+            title:"Is Admin",
+            type:"boolean",
+            description:"Check if user is admin",
+            initialValue:false,
+            validation:(Rule)=>Rule.required(),
+            //readOnly:true,
+            //hidden:true,
+        }),
+        defineField({
+            name:"name",
+            title:"Name",
+            type:"string",
+            description:"Name of the user",
+            validation:(Rule)=>Rule.required(),
+        }),
+        defineField({
+            name:"image",
+            title:"Image",
+            type:"url",
+        }),
+        defineField({
+            name:"password",
+            type:"string",
+            hidden: true,
+            validation:(Rule)=>Rule.required(),
+        }),
+        defineField({
+            name:'email',
+            title:'Email',
+            type:'string',
+        }),
+        defineField({
+            name:'emailVerified',
+            hidden:true,
+            type:'datetime',
+        }),
+        defineField({
+            name:"about",
+            title:"About",
+            type:"text",
+            description:"Brief description about user",
+        }),
+    ],
+};
+
+export default user;
