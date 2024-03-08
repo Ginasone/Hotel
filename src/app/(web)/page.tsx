5761b6674083205ebd1b0d61a6cdf453a612d65d
Ginasone <127901244+Ginasone@users.noreply.github.com>
2024-03-08 14:28:50 +0000

    Add files via upload

---
+++ src/app/(web)/page.tsx
@@ -0,0 +1,16 @@
+import HeroSection from "../components/HeroSection/HeroSection";
+import PageSearch from "../components/PageSearch/PageSearch";
+
+const Home =() => {
+  return (
+    <>
+      <HeroSection />
+      <PageSearch />
+      {/* Featured Room */}
+      {/* Gallery*/}
+      {/* News Letter */}
+    </>
+  );
+};
+
+export default Home;
